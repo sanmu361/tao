510f30cc992ab7b19255d5eef8e43c612e17db92
sanmu361 <1535958735@qq.com>
2018-07-18 00:33:30 +0800

    环境配置

--- tao_common/src/test/java/com/sanmu/tao/IDUtils/FtpUtilTest.java
+++ tao_common/src/test/java/com/sanmu/tao/IDUtils/FtpUtilTest.java
@@ -18,32 +18,32 @@ import static org.junit.Assert.*;
  **/
 public class FtpUtilTest {
 
-    @Test
-    public void uploadFile() throws IOException {
-
-                String FTP_ADDRESS="192.168.181.125";
-        int FTP_PORT=21;
-        String FTP_USERNAME="root";
-        String FTP_PASSWORD="123456";
-        String FTP_BASE_PATH="/images";
-
-        String IMAGE_BASE_URL="http://192.168.181.125:8080/images";
-
-        File uploadFile = new File("D:\\SC01C25.jpg");
-
-        //生成一个新的文件名
-        //取原始文件名
-        String oldName = "SC01C25.jpg";
-        //生成新文件名
-        //UUID.randomUUID();
-        String newName = IDUtils.getFileNmae();
-        newName = newName + oldName.substring(oldName.lastIndexOf("."));
-        //图片上传
-        String imagePath = new DateTime().toString("/yyyy/MM/dd");
-        FileInputStream inputStream = new FileInputStream(uploadFile);
-        boolean result = FtpUtil.uploadFile(FTP_ADDRESS, FTP_PORT, FTP_USERNAME, FTP_PASSWORD,
-                FTP_BASE_PATH, imagePath, newName,inputStream);
-
-        System.out.println(result);
-    }
+//    @Test
+//    public void uploadFile() throws IOException {
+//
+//                String FTP_ADDRESS="192.168.181.125";
+//        int FTP_PORT=21;
+//        String FTP_USERNAME="root";
+//        String FTP_PASSWORD="123456";
+//        String FTP_BASE_PATH="/images";
+//
+//        String IMAGE_BASE_URL="http://192.168.181.125:8080/images";
+//
+//        File uploadFile = new File("D:\\SC01C25.jpg");
+//
+//        //生成一个新的文件名
+//        //取原始文件名
+//        String oldName = "SC01C25.jpg";
+//        //生成新文件名
+//        //UUID.randomUUID();
+//        String newName = IDUtils.getFileNmae();
+//        newName = newName + oldName.substring(oldName.lastIndexOf("."));
+//        //图片上传
+//        String imagePath = new DateTime().toString("/yyyy/MM/dd");
+//        FileInputStream inputStream = new FileInputStream(uploadFile);
+//        boolean result = FtpUtil.uploadFile(FTP_ADDRESS, FTP_PORT, FTP_USERNAME, FTP_PASSWORD,
+//                FTP_BASE_PATH, imagePath, newName,inputStream);
+//
+//        System.out.println(result);
+//    }
 }

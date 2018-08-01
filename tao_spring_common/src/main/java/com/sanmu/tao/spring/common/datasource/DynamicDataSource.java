4c9c4781c8f70820c46b51e5b45caeb4159c5284
sanmu361 <1535958735@qq.com>
2018-08-01 12:20:28 +0800

    Update DynamicDataSource.java

--- tao_spring_common/src/main/java/com/sanmu/tao/spring/common/datasource/DynamicDataSource.java
+++ tao_spring_common/src/main/java/com/sanmu/tao/spring/common/datasource/DynamicDataSource.java
@@ -6,7 +6,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * ${DESCRIPTION}
  *
  * @author yansen
- * @create 2018-07-18 16:16
+ * @create 2016-07-18 16:16
  **/
 public class DynamicDataSource extends AbstractRoutingDataSource {
     @Override
